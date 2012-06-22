b334cc0a40c3a0c0b14b3e783662e3bb0494b3ae
dojo <dojo@agilept.org>
2012-06-22 17:30:40 +0100

    Test valid moviment

--- dojo.py
+++ dojo.py
@@ -3,4 +3,4 @@ class TTTGame(object):
     x = "player1"
 
     def make_move(self,player, posx, posy):
-        pass
+        return True
--- tests.py
+++ tests.py
@@ -10,6 +10,11 @@ class TestDojo(unittest.TestCase):
         game = TTTGame()
         game.make_move(TTTGame.x, 1, 1)
 
+    def test_execute_move_valid(self):
+        game = TTTGame()
+        result = game.make_move(TTTGame.x, 1, 1)
+        self.assertEqual(result, True)
+
 
 if __name__ == '__main__':
     unittest.main()
