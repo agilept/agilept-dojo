842f053736daec208e6af862289e35545acbabf6
unknown <secdei@rvidal-PC.(none)>
2012-06-22 17:21:51 +0100

    Teste da board a 0

---
+++ agilept2012-dojo/TTTGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace agilept2012
+{
+    class TTTGame
+    {
+
+
+        static void Main(string[] args)
+        {
+        }
+
+        private ArrayList board;
+
+        public TTTGame()
+        {
+            board = new ArrayList();
+        }
+
+        internal ArrayList getBoard()
+        {
+            return board;
+        }
+    }
+}
---
+++ agilept2012-dojo/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using System.Collections;
+
+namespace agilept2012
+{
+    [TestFixture]
+    class TestTicTacToe
+    {
+
+        [Test]
+        public void testBoard() {
+            ArrayList board = new TTTGame().getBoard();
+            foreach (int b in board)
+            {
+                Assert.AreEqual(0, b);
+            }
+        }
+    }
+}
