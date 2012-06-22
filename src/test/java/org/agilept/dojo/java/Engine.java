5492563870b5574cd7195e1d8a170ef3d9c2ab8b
dojo <dojo@agilept.org>
2012-06-22 17:31:59 +0100

    teste inicial

---
+++ src/test/java/org/agilept/dojo/java/Engine.java
@@ -0,0 +1,21 @@
+package org.agilept.dojo.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Engine {
+	
+	int[][] plays;
+	
+	public Engine(){
+		plays = new int[][]{{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
+			
+	}
+
+	public int[][] getMatrix() {
+		return plays;
+	}
+	
+	
+
+}
--- src/test/java/org/agilept/dojo/java/Test.java
+++ src/test/java/org/agilept/dojo/java/Test.java
@@ -1,12 +1,19 @@
 package org.agilept.dojo.java;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Test {
 
 	@org.junit.Test
-	public void test(){
-	
+	public void testEngineCreation(){
+		Engine engine = new Engine();
+		int[][] plays = engine.getMatrix();
+		for(int[] play : plays)
+			for(int x : play)
+				assertEquals(-1, x);
 	}
 
 }
