7ba246249e05c2f6e6d66cbb6567a9a83052b918
dojo <dojo@agilept.org>
2012-06-22 17:45:35 +0100

    Test valid movement passing

--- dojo.py
+++ dojo.py
@@ -1,6 +1,15 @@
 class TTTGame(object):
 
     x = "player1"
+    o = "player2"
+
+    def __init__(self):
+        self.plays = [[0,0,0],[0,0,0],[0,0,0]]
 
     def make_move(self,player, posx, posy):
-        return True
+        print self.plays[posx][posy]
+        if self.plays[posx][posy] == 0:
+            self.plays[posx][posy] = player
+            return True
+        else:
+            return False
--- tests.py
+++ tests.py
@@ -6,14 +6,17 @@ class TestDojo(unittest.TestCase):
         game = TTTGame()
         self.assertNotEqual(game, None)
 
-    def test_make_move(self):
-        game = TTTGame()
-        game.make_move(TTTGame.x, 1, 1)
-
     def test_execute_move_valid(self):
         game = TTTGame()
         result = game.make_move(TTTGame.x, 1, 1)
         self.assertEqual(result, True)
+
+    def test_execute_move_invalid(self):
+        game = TTTGame()
+        result = game.make_move(TTTGame.x, 1, 1)
+        self.assertEqual(result, True)
+        result = game.make_move(TTTGame.x, 1, 1)
+        self.assertEqual(result, False)
 
 
 if __name__ == '__main__':
